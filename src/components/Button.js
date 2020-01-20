f262edfd9108f25e04fb0fadbefc877d284ec555
jiselle <jiselle.santana@gfxconsultoria.com>
2020-01-20 17:51:19 -0300

    criando os botoes e adicionando o display

---
+++ src/components/Button.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { StyleSheet, View, Text, Dimensions, TouchableHighlight } from 'react-native';
+
+export default props => {
+    const stylesButton = [style.button]
+    if(props.double) stylesButton.push(style.buttonDouble)
+    if(props.triple) stylesButton.push(style.buttonTriple)
+    if(props.operation) stylesButton.push(style.operationButton)
+
+    return (
+        <TouchableHighlight onPress={props.onclick}>
+            <Text style={style.stylesButton}>
+                {props.label}
+            </Text>
+        </TouchableHighlight>
+    )
+}
+const style = StyleSheet.create({
+    button: {
+        fontSize: 40,
+        height: Dimensions.get('window').width / 4,
+        width: Dimensions.get('window').width / 4,
+        padding: 20,
+        backgroundColor: '#f0f0f0',
+        textAlign: 'center',
+        borderWidth: 1,
+        borderColor: '#888',
+    },
+    operationButton: {
+        color: '#fff',
+        backgroundColor: '#ffb6c1'
+    },
+    buttonDouble: {
+        width: (Dimensions.get('window').width / 4) * 2,
+    },
+    buttonTriple: {
+        width: (Dimensions.get('window').width / 4) * 3,
+    }
+})
--- src/index.js
+++ src/index.js
@@ -1,12 +1,60 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import Button from './components/Button';
+import Display from './components/Display';
 
 export default class App extends React.Component {
+
+    state = {
+        displayValue: '0'
+    }
+
+    addDigit = n => {
+        this.setState({ displayValue: n })
+    }
+
+    clearMemory = () => {
+        this.setState({ displayValue: '0' })
+    }
+
+    setOperation = operation => {
+
+    }
+
     render() {
         return (
-            <View>
-
+            <View style={style.container}>
+                <Display value={this.state.displayValue} />
+                <View style={style.buttons}>
+                    <Button label='AC' triple onClick={this.clearMemory} ></Button>
+                    <Button label='/'></Button>
+                    <Button label='7'></Button>
+                    <Button label='8'></Button>
+                    <Button label='9'></Button>
+                    <Button label='*'></Button>
+                    <Button label='4'></Button>
+                    <Button label='5'></Button>
+                    <Button label='6'></Button>
+                    <Button label='-'></Button>
+                    <Button label='1'></Button>
+                    <Button label='2'></Button>
+                    <Button label='3'></Button>
+                    <Button label='+'></Button>
+                    <Button label='0'></Button>
+                    <Button label='.'></Button>
+                    <Button label='='></Button>
+                </View>
             </View>
         )
     }
 }
+
+const style = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    buttons: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+    }
+})
